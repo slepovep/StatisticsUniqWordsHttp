760bd568df2f47d34ae712ddc3f5954c961e0ea0
Евгений Слепов (NB) <slepov.ep@gmail.com>
2021-04-06 01:08:20 +0300

    add insert db

--- StatisticsUniqWordsHttp/CRUD.cs
+++ StatisticsUniqWordsHttp/CRUD.cs
@@ -8,6 +8,34 @@ namespace StatisticsUniqWordsHttp
 {
     public class CRUD
     {
+      public static void AddRequest ()
+	  {
+
+            using (AppContext db = new AppContext())
+            {
+                // создаем два объекта User
+                RequestStats request1 = new RequestStats { User = "User1", RequestTime = DateTime.Now, UrlPage = "webpage_url" };
+                RequestStats request2 = new RequestStats { User = "User2", RequestTime = DateTime.Now, UrlPage = "webpage_url2" };
+
+                // добавляем их в бд
+                db.RequestStat.Add(request1);
+                db.RequestStat.Add(request2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+ 
+                // получаем объекты из бд и выводим на консоль
+                var req = db.RequestStat.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (RequestStats r in req)
+                {
+                    Console.WriteLine($"{r.Id}.{r.User} {r.RequestTime} {r.UrlPage}");
+                }
+            }
+
+
+
+
+	  }
 
 
     }
--- StatisticsUniqWordsHttp/Program.cs
+++ StatisticsUniqWordsHttp/Program.cs
@@ -6,15 +6,18 @@ namespace StatisticsUniqWordsHttp
 	{
 		static void Main(string[] args)
 		{
-			WebPage webpage = new WebPage();
-			webpage.UrlPage = "https://www.simbirsoft.com/";
-			webpage.FileName = "test.html";
-			//скачивание web-страницы на локальный диск
-			webpage.DownloadFile();
+			/*	WebPage webpage = new WebPage();
+				webpage.UrlPage = "https://www.simbirsoft.com/";
+				webpage.FileName = "test.html";
+				//скачивание web-страницы на локальный диск
+				webpage.DownloadFile();
 
-			StatisticFile file = new StatisticFile();
-			//вывод статистики по количеству уникальных слов
-			file.CountWords(webpage.LocalFile);
+				StatisticFile file = new StatisticFile();
+				//вывод статистики по количеству уникальных слов
+				file.CountWords(webpage.LocalFile);
+
+			*/
+			CRUD.AddRequest();
 			Console.ReadKey();
 		}
 	}
