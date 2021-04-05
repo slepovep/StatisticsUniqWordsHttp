0fe4e303360ba466e4b847785ccc02e656b9cf90
slepovep <slepov.ep@gmail.com>
2021-04-05 18:46:22 +0300

    add Database

---
+++ StatisticsUniqWordsHttp/AppContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StatisticsUniqWordsHttp
+{
+    public class AppContext : DbContext
+    {
+        public DbSet<RequestStats> RequestStat { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=statdb;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ StatisticsUniqWordsHttp/RequestStats.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatisticsUniqWordsHttp
+{
+    public class RequestStats
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public string UrlPage { get; set; }
+        public DateTime RequestTime { get; set; }
+    }
+}
